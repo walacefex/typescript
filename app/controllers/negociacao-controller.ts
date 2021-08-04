53fb7597e3ea40cee1cb5e5c7ea83b767141b4d5
walacefex <walacefex01@gmail.com>
2021-08-04 02:49:41 -0300

    Vendo Questões de mutabilidade e como solucioná-la, revisando encapsulamento e aplicando ReadonlyArray para somente ler na lista e não alterar

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController{
   private inputData: HTMLInputElement;
   private inputQuantidade:HTMLInputElement;
   private inputValor:HTMLInputElement;
+  private negociacoes: Negociacoes = new Negociacoes();
 
   constructor(){
     this.inputData = document.querySelector('#data');
@@ -13,7 +15,9 @@ export class NegociacaoController{
 
   adiciona(): void{
     const negociacao = this.criarNegociacao();
-    console.log(negociacao);
+    this.negociacoes.adiciona(negociacao);
+    this.negociacoes.lista().pop();
+    console.log(this.negociacoes.lista())
     this.limparFormulario()
   }
 
@@ -29,6 +33,6 @@ export class NegociacaoController{
     this.inputData.value = '';
     this.inputQuantidade.value = '';
     this.inputValor.value = '';
-    this.inputData.focus()
+    this.inputData.focus();
   }
 }
